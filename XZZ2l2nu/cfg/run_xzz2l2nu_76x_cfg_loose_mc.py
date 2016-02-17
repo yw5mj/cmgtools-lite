e253c1fb0e91ef695e919c7855be514dd29fab5e
Hengne <Hengne.Li@CERN.CH>
2016-02-17 18:56:05 +0100

    change to use AAA for ZZTo2L2Nu because this set is missing in cern EOS

--- XZZ2l2nu/cfg/run_xzz2l2nu_76x_cfg_loose_mc.py
+++ XZZ2l2nu/cfg/run_xzz2l2nu_76x_cfg_loose_mc.py
@@ -67,7 +67,7 @@ if test==1:
     #selectedComponents = [RSGravToZZToZZinv_narrow_800]
     #selectedComponents = [DYJetsToLL_M50_Ext]
     #selectedComponents = [WWToLNuQQ, WZTo1L1Nu2Q, WZTo2L2Q, ZZTo2L2Nu]
-    selectedComponents = [WZTo2L2Q]
+    selectedComponents = [ZZTo2L2Nu]
     #selectedComponents = [BulkGravToZZ_narrow_800]
     #selectedComponents = [BulkGravToZZToZlepZhad_narrow_800]
     for c in selectedComponents:
