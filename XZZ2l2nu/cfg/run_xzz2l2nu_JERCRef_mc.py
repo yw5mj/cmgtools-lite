2bcd3a28ba708165cba317c9787706808c61f96f
Wu <mewu@cern.ch>
2016-04-12 15:20:04 +0200

    met corrected after JER correction

--- XZZ2l2nu/cfg/run_xzz2l2nu_JERCRef_mc.py
+++ XZZ2l2nu/cfg/run_xzz2l2nu_JERCRef_mc.py
@@ -35,7 +35,7 @@ lepAna.electronIsoVersion = 'miniISO' # can be pfISO or miniISO
 lepAna.do_filter=False
 
 jetAna.smearJets=True
-#metAna.applyJetSmearing=True
+metAna.applyJetSmearing=True
 #metAna.old74XMiniAODs = True
 
 #-------- SEQUENCE
