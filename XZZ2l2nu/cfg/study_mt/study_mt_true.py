6dd8326671ffd3584840f3fee17d96ec950d3150
Hengne <Hengne.Li@CERN.CH>
2016-05-09 00:15:46 +0200

    add truth mt study

--- XZZ2l2nu/cfg/study_mt/study_mt_true.py
+++ XZZ2l2nu/cfg/study_mt/study_mt_true.py
@@ -101,7 +101,7 @@ sigXsec = {
 'BulkGravToZZToZlepZinv_narrow_4500' : 1,
 }
 sigMassWindow = {
-'BulkGravToZZToZlepZinv_narrow_600' : [60,200,700],
+'BulkGravToZZToZlepZinv_narrow_600' : [60,560,620],
 'BulkGravToZZToZlepZinv_narrow_800' : [60,400,1000],
 'BulkGravToZZToZlepZinv_narrow_1000' : [80,400,1200],
 'BulkGravToZZToZlepZinv_narrow_1200' : [100,400,1400],
@@ -125,12 +125,12 @@ for sample in sigSamples:
 
 
 
-fout = ROOT.TFile("study_mt.root", 'recreate')
+fout = ROOT.TFile("study_mt_true.root", 'recreate')
 
 c1 = ROOT.TCanvas("c1","c1")
 c1.SetBottomMargin(0.15)
 c1.SetLeftMargin(0.15)
-c1.Print("study_mt.ps[")
+c1.Print("study_mt_true.ps[")
 
 h1 = []
 h2 = []
@@ -142,10 +142,10 @@ for idx,sample in enumerate(sigSamples):
     nbins = sigMassWindow[sample][0]
     xmin = sigMassWindow[sample][1]
     xmax = sigMassWindow[sample][2]
-    h1.append(sigPlotters[idx].drawTH1(sample+'_hmt','llnunu_mt',cuts,str(lumi*1000),nbins,xmin,xmax,titlex = "M_{T}",units = "GeV"))
-    h2.append(sigPlotters[idx].drawTH1(sample+'_hmta','llnunu_mta',cuts,str(lumi*1000),nbins,xmin,xmax,titlex = "M_{T}",units = "GeV"))
-    h3.append(sigPlotters[idx].drawTH1(sample+'_hmtb','llnunu_mtb',cuts,str(lumi*1000),nbins,xmin,xmax,titlex = "M_{T}",units = "GeV"))
-    h4.append(sigPlotters[idx].drawTH1(sample+'_hmtc','llnunu_mtc',cuts,str(lumi*1000),nbins,xmin,xmax,titlex = "M_{T}",units = "GeV"))
+    h1.append(sigPlotters[idx].drawTH1(sample+'_hmt','genX_mt',cuts,str(lumi*1000),nbins,xmin,xmax,titlex = "True M_{T}",units = "GeV"))
+    h2.append(sigPlotters[idx].drawTH1(sample+'_hmta','genX_mta',cuts,str(lumi*1000),nbins,xmin,xmax,titlex = "True M_{T}",units = "GeV"))
+    h3.append(sigPlotters[idx].drawTH1(sample+'_hmtb','genX_mtb',cuts,str(lumi*1000),nbins,xmin,xmax,titlex = "True M_{T}",units = "GeV"))
+    h4.append(sigPlotters[idx].drawTH1(sample+'_hmtc','genX_mtc',cuts,str(lumi*1000),nbins,xmin,xmax,titlex = "True M_{T}",units = "GeV"))
 
     h1[idx].SetMarkerSize(0.5)
     h2[idx].SetMarkerSize(0.5)
@@ -161,18 +161,24 @@ for idx,sample in enumerate(sigSamples):
     h4[idx].SetLineColor(8)
     h4[idx].SetMarkerColor(8)
 
-    lg.append(ROOT.TLegend(0.2,0.6,0.6,0.8))
-    lg[idx].SetName(sample+'_lg')
-    lg[idx].AddEntry(h1[idx],"M(#nu#nu) = 0 GeV","pl")
+    lg.append(ROOT.TLegend(0.2,0.5,0.65,0.8))
+    #lg[idx].SetName(sample+'_lg')
+    #lg[idx].AddEntry(h1[idx],"MT","pl")
+    #lg[idx].AddEntry(h2[idx],"MTa","pl")
+    ##lg[idx].AddEntry(h3[idx],"MTb","pl")
+    #lg[idx].AddEntry(h4[idx],"MTc","pl")
+
+    lg[idx].AddEntry(h1[idx],"M(#nu#nu) = True M(#nu#nu)","pl")
     lg[idx].AddEntry(h2[idx],"M(#nu#nu) = 91.188 GeV","pl")
-    lg[idx].AddEntry(h4[idx],"M(#nu#nu) = M(ll)","pl")
+    lg[idx].AddEntry(h4[idx],"M(#nu#nu) = True M(ll)","pl")
 
     c1.Clear()
-    h2[idx].Draw('l hist')
-    h1[idx].Draw('l hist same')
+    h1[idx].Draw('l hist')
+    h2[idx].Draw('l hist same')
+    #h3[idx].Draw('l hist same')
     h4[idx].Draw('l hist same')
     lg[idx].Draw()
-    c1.Print("study_mt.ps")
+    c1.Print("study_mt_true.ps")
     c1.Clear()
 
     fout.cd()
@@ -182,9 +188,9 @@ for idx,sample in enumerate(sigSamples):
     h4[idx].Write()
     lg[idx].Write()
 
-c1.Print("study_mt.ps]")
+c1.Print("study_mt_true.ps]")
 
-os.system('ps2pdf study_mt.ps')
+os.system('ps2pdf study_mt_true.ps')
 fout.Close()
 
 
