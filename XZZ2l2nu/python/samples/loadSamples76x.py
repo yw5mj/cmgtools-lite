4d1205bfc6c20e6d53849b3b452b3a9b79c08d2c
Wu <mewu@cern.ch>
2016-05-24 16:49:33 +0200

    background samples changed

--- XZZ2l2nu/python/samples/loadSamples76x.py
+++ XZZ2l2nu/python/samples/loadSamples76x.py
@@ -22,18 +22,10 @@ jercRefdt=[JERCRef_data]
 
 # backgrounds
 backgroundSamples = [
-# DYJetsToLL_M50_HT100to200,
-# DYJetsToLL_M50_HT200to400,
-# DYJetsToLL_M50_HT400to600,
-# DYJetsToLL_M50_HT600toInf,
-WW,
-WZ,
-ZZ,
 DYJetsToLL_M50,
 DYJetsToLL_M50_Ext,
 DYJetsToLL_M50_PtZ100,
 WJetsToLNu,
-TT,
 TTTo2L2Nu,
 ZZTo2L2Nu,
 ZZTo4L,
@@ -47,6 +39,16 @@ TTZToLLNuNu,
 TTWJetsToLNu,
 ]
 
+extraBackgroundMC = [
+# DYJetsToLL_M50_HT100to200,
+# DYJetsToLL_M50_HT200to400,
+# DYJetsToLL_M50_HT400to600,
+# DYJetsToLL_M50_HT600toInf,
+WW,
+WZ,
+ZZ,
+TT,
+]
 
 # signals
 signalSamples = [
@@ -80,12 +82,11 @@ BulkGravToZZToZlepZhad_narrow_3500,
 BulkGravToZZToZlepZhad_narrow_4000,
 BulkGravToZZToZlepZhad_narrow_4500,
 ]
-# mc samples
-otherMcSamples=BulkGravToZZToZlepZhad  
+
+# MC samples
+otherMcSamples = BulkGravToZZToZlepZhad  
 mcSamples = signalSamples + backgroundSamples + jercRefMC
 #mcSamples = backgroundSamples
-
-
 
 # data
 SingleMuon=[SingleMuon_Run2015C_25ns_16Dec,SingleMuon_Run2015D_16Dec]
