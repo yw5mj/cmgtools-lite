61479ee1c9bf08ae45ca0fe3d1437c7107636fbb
Hengne <Hengne.Li@CERN.CH>
2016-05-29 18:11:14 +0200

    clean up some non-common collections in output tree, keep useful generator level information, for particular studies, please create dedicated config file to include the needed collections.

--- XZZ2l2nu/python/analyzers/treeXZZ_cff.py
+++ XZZ2l2nu/python/analyzers/treeXZZ_cff.py
@@ -59,7 +59,7 @@ vvTreeProducer = cfg.Analyzer(
          NTupleVariable("triggersfLo",  lambda x : getattr(x,'trgsfLo',1), help="singleelectron/muon trigger sf lower"),
      ],
      globalObjects =  {
-         "met" : NTupleObject("met", metType, help="PF E_{T}^{miss}, after type 1 corrections"),
+         #"met" : NTupleObject("met", metType, help="PF E_{T}^{miss}, after type 1 corrections"),
          #"met_miniAod" : NTupleObject("met_raw", metType, help="PF E_{T}^{miss}, after type 1 corrections in miniAOD"),
          #"met_JEC" : NTupleObject("met_JEC", metType, help="PF E_{T}^{miss}, after type 1 corrections with new 76X JEC added"),
          "met_JECUp" : NTupleObject("met_JECUp", metType, help="PF E_{T}^{miss}, after type 1 corrections with new 76X JEC up"),
@@ -69,13 +69,13 @@ vvTreeProducer = cfg.Analyzer(
 
      collections = {
          #"LL"  : NTupleCollection("Zll",LLType,5, help="Z to ll"),
-         "ElMu"  : NTupleCollection("elmu",LLType,5, help="electron - muon pair for non-resonant bkg"),
-         "selectedLeptons" : NTupleCollection("lep",leptonType,10, help="selected leptons"),
+         #"ElMu"  : NTupleCollection("elmu",LLType,5, help="electron - muon pair for non-resonant bkg"),
+         #"selectedLeptons" : NTupleCollection("lep",leptonType,10, help="selected leptons"),
          "genLeptons" : NTupleCollection("genLep", genParticleType, 10, help="Generated leptons (e/mu) from W/Z decays"),
-         #"genZBosons" : NTupleCollection("genZ", genParticleType, 10, help="Generated V bosons"),
+         "genZBosons" : NTupleCollection("genZ", genParticleType, 10, help="Generated V bosons"),
          "LLNuNu"     : NTupleCollection("llnunu",LLNuNuType ,5, help="VV candidate with di-lepton and MET"),
-         #"genXZZ" : NTupleCollection("genX", VVType, 10, mcOnly=True, help="Generated X->ZZ"),
-         "LHE_weights"    : NTupleCollection("LHEweight",  weightsInfoType, 1000, help="LHE weight info"),
+         "genXZZ" : NTupleCollection("genX", VVType, 10, mcOnly=True, help="Generated X->ZZ"),
+         #"LHE_weights"    : NTupleCollection("LHEweight",  weightsInfoType, 1000, help="LHE weight info"),
          #"jets"       : NTupleCollection("jet_corr",corrJetType,15, help="all jets with new JEC for 76X applied (JER corrected if isMC)"),
          #"jets_raw"   : NTupleCollection("jet",JetType,15, help="all jets from miniAOD"),
      }
