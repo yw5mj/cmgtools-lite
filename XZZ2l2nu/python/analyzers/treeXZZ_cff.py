13e0501d71dde0b103bed4ae73d570e2bcf6df03
Hengne <Hengne.Li@CERN.CH>
2016-06-03 03:37:33 +0200

    add jets in default output and remove LHEweights

--- XZZ2l2nu/python/analyzers/treeXZZ_cff.py
+++ XZZ2l2nu/python/analyzers/treeXZZ_cff.py
@@ -83,8 +83,8 @@ vvTreeProducer = cfg.Analyzer(
          "genZBosons" : NTupleCollection("genZ", genParticleType, 10, help="Generated V bosons"),
          "LLNuNu"     : NTupleCollection("llnunu",LLNuNuType ,5, help="VV candidate with di-lepton and MET"),
          "genXZZ" : NTupleCollection("genX", VVType, 10, mcOnly=True, help="Generated X->ZZ"),
-         "LHE_weights"    : NTupleCollection("LHEweight",  weightsInfoType, 1000, help="LHE weight info"),
-         #"jets"       : NTupleCollection("jet_corr",jetType,15, help="all jets with new JEC for 76X applied"),
+         #"LHE_weights"    : NTupleCollection("LHEweight",  weightsInfoType, 1000, help="LHE weight info"),
+         "jets"       : NTupleCollection("jet_corr",jetType,15, help="all jets with new JEC for 76X applied"),
          #"jets_raw"   : NTupleCollection("jet",JetType,15, help="all jets from miniAOD"),
      }
 )
