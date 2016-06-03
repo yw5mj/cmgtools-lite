3e07a4abdf76accfdee195703485ff0a90e0ff55
Hengne <Hengne.Li@CERN.CH>
2016-06-03 07:12:02 +0200

    add samples

---
+++ XZZ2l2nu/python/samples/loadSamples80x.py
@@ -0,0 +1,138 @@
+#####################
+# load samples 
+#####################
+
+import PhysicsTools.HeppyCore.framework.config as cfg
+import os
+
+# Load backgrounds
+from CMGTools.XZZ2l2nu.samples.samples_13TeV_RunIISpring16MiniAODv2 import *
+# Load signals
+from CMGTools.XZZ2l2nu.samples.samples_13TeV_signal76X import *
+# Load Data 
+from CMGTools.XZZ2l2nu.samples.samples_13TeV_DATA2016 import *
+# Load triggers
+from CMGTools.XZZ2l2nu.samples.triggers_13TeV_Spring15 import *
+# Load reference sample for jet energy corrections and jet resolution
+from CMGTools.XZZ2l2nu.samples.samples_JERCReference import * 
+
+# mc reference sample for jet energy corrections and jet resolution
+jercRefMC=[JERCRef_MC, JERCRef_MC_eos]
+jercRefdt=[JERCRef_data]
+
+# backgrounds
+backgroundSamples = [
+DYJetsToLL_M50,
+DYJetsToLL_M50_Ext,
+DYJetsToLL_M50_PtZ100,
+WJetsToLNu,
+TTTo2L2Nu,
+ZZTo2L2Nu,
+ZZTo4L,
+ZZTo2L2Q,
+WWTo2L2Nu,
+WWToLNuQQ,
+WZTo1L1Nu2Q,
+WZTo2L2Q,
+WZTo3LNu,
+TTZToLLNuNu,
+TTWJetsToLNu,
+ggZZTo2e2nu,
+ggZZTo2mu2nu,
+]
+
+extraBackgroundMC = [
+# DYJetsToLL_M50_HT100to200,
+# DYJetsToLL_M50_HT200to400,
+# DYJetsToLL_M50_HT400to600,
+# DYJetsToLL_M50_HT600toInf,
+WW,
+WZ,
+ZZ,
+TT,
+]
+
+# signals
+signalSamples = [
+BulkGravToZZToZlepZinv_narrow_600,
+BulkGravToZZToZlepZinv_narrow_800,
+BulkGravToZZToZlepZinv_narrow_1000,
+BulkGravToZZToZlepZinv_narrow_1200,
+BulkGravToZZToZlepZinv_narrow_1400,
+BulkGravToZZToZlepZinv_narrow_1600,
+BulkGravToZZToZlepZinv_narrow_1800,
+BulkGravToZZToZlepZinv_narrow_2000,
+BulkGravToZZToZlepZinv_narrow_2500,
+BulkGravToZZToZlepZinv_narrow_3000,
+BulkGravToZZToZlepZinv_narrow_3500,
+BulkGravToZZToZlepZinv_narrow_4000,
+BulkGravToZZToZlepZinv_narrow_4500,
+]
+
+BulkGravToZZToZlepZhad = [
+BulkGravToZZToZlepZhad_narrow_600,
+BulkGravToZZToZlepZhad_narrow_800,
+BulkGravToZZToZlepZhad_narrow_1000,
+BulkGravToZZToZlepZhad_narrow_1200,
+BulkGravToZZToZlepZhad_narrow_1400,
+BulkGravToZZToZlepZhad_narrow_1600,
+BulkGravToZZToZlepZhad_narrow_1800,
+BulkGravToZZToZlepZhad_narrow_2000,
+BulkGravToZZToZlepZhad_narrow_2500,
+BulkGravToZZToZlepZhad_narrow_3000,
+BulkGravToZZToZlepZhad_narrow_3500,
+BulkGravToZZToZlepZhad_narrow_4000,
+BulkGravToZZToZlepZhad_narrow_4500,
+]
+
+# MC samples
+otherMcSamples = BulkGravToZZToZlepZhad  
+mcSamples = signalSamples + backgroundSamples + jercRefMC + extraBackgroundMC
+#mcSamples = backgroundSamples
+
+# data
+SingleMuon=[SingleMuon_Run2016B_PromptReco_v2]
+SingleElectron=[SingleElectron_Run2016B_PromptReco_v2]
+SinglePhoton=[SinglePhoton_Run2016B_PromptReco_v2]
+MuEG=[MuonEG_Run2016B_PromptReco_v2]
+
+for s in SingleMuon:
+    s.triggers = triggers_1mu_noniso
+    s.vetoTriggers = []
+for s in SingleElectron:
+    s.triggers = triggers_1e_noniso
+    s.vetoTriggers = triggers_1mu_noniso
+for s in SinglePhoton:
+    s.trigers = triggers_photon_unbias
+
+
+dataSamples=SingleMuon+SingleElectron #+jercRefdt
+
+# JSON
+silverJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_Silver.txt'
+goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-273730_13TeV_PromptReco_Collisions16_JSON.txt'
+
+jsonFile = goldenJson
+
+
+from CMGTools.XZZ2l2nu.setup.Efficiencies import *
+dataDir = "$CMSSW_BASE/src/CMGTools/XZZ2l2nu/data"
+
+for comp in mcSamples+otherMcSamples:
+    comp.isMC = True
+    comp.isData = False
+    comp.splitFactor = 250
+    comp.puFileMC=dataDir+"/pileup_MC_76x.root"
+    comp.puFileData=dataDir+"/pileup_DATA.root"
+    comp.eSFinput=dataDir+"/CutBasedID_LooseWP_76X_18Feb.txt_SF2D.root"
+    comp.efficiency = eff2012
+    comp.triggers=triggers_1mu_noniso+triggers_1e_noniso
+    comp.globalTag = "Summer15_25nsV6_MC"
+
+for comp in dataSamples:
+    comp.splitFactor = 250
+    comp.isMC = False
+    comp.isData = True
+    comp.json = jsonFile
+    comp.globalTag = "Summer15_25nsV6_DATA"
+
--- XZZ2l2nu/python/samples/samples_13TeV_DATA2016.py
+++ XZZ2l2nu/python/samples/samples_13TeV_DATA2016.py
@@ -8,12 +8,12 @@ kreator = ComponentCreator()
 
 
 # Hengne:
-# /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-273730_13TeV_PromptReco_Collisions16_JSON.txt
+json=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-273730_13TeV_PromptReco_Collisions16_JSON.txt
 
 ### ----------------------------- Zero Tesla run  ----------------------------------------
 
-dataDir = "$CMSSW_BASE/src/CMGTools/TTHAnalysis/data"  # use environmental variable, useful for instance to run on CRAB
-json=dataDir+'/json/Cert_246908-256869_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+#dataDir = "$CMSSW_BASE/src/CMGTools/TTHAnalysis/data"  # use environmental variable, useful for instance to run on CRAB
+#json=dataDir+'/json/Cert_246908-256869_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
 #https://hypernews.cern.ch/HyperNews/CMS/get/physics-validation/2496.html
 #golden JSON 166.37/pb 
 
@@ -30,8 +30,8 @@ json=dataDir+'/json/Cert_246908-256869_13TeV_PromptReco_Collisions15_25ns_JSON.t
 
 
 # PromptReco-v1 for run > 251561
-run_range = (251643, 251883)
-label = "_runs%s_%s"%(run_range[0], run_range[1])
+#run_range = (251643, 251883)
+#label = "_runs%s_%s"%(run_range[0], run_range[1])
 
 ### ----------------------------- Run2016 PromptReco v1 ----------------------------------------
 
@@ -63,41 +63,5 @@ dataSamples_Run2016_v2 = [JetHT_Run2016B_PromptReco_v2, HTMHT_Run2016B_PromptRec
 
 ### ----------------------------- summary ----------------------------------------
 
-dataSamples_PromptReco = dataSamples_Run2016_v1 + dataSamples_Run2016_v2
-samples = dataSamples_PromptReco
 
 ### ---------------------------------------------------------------------
-
-from CMGTools.TTHAnalysis.setup.Efficiencies import *
-dataDir = "$CMSSW_BASE/src/CMGTools/TTHAnalysis/data"
-
-for comp in samples:
-    comp.splitFactor = 1000
-    comp.isMC = False
-    comp.isData = True
-
-if __name__ == "__main__":
-   import sys
-   if "test" in sys.argv:
-       from CMGTools.RootTools.samples.ComponentCreator import testSamples
-       testSamples(samples)
-if __name__ == "__main__":
-   import sys
-   if "test" in sys.argv:
-       from CMGTools.RootTools.samples.ComponentCreator import testSamples
-       testSamples(samples)
-   if "locality" in sys.argv:
-       import re
-       from CMGTools.Production.localityChecker import LocalityChecker
-       tier2Checker = LocalityChecker("T2_CH_CERN", datasets="/*/*/MINIAOD*")
-       for comp in samples:
-           if len(comp.files) == 0: 
-               print '\033[34mE: Empty component: '+comp.name+'\033[0m'
-               continue
-           if not hasattr(comp,'dataset'): continue
-           if not re.match("/[^/]+/[^/]+/MINIAOD(SIM)?", comp.dataset): continue
-           if "/store/" not in comp.files[0]: continue
-           if re.search("/store/(group|user|cmst3)/", comp.files[0]): continue
-           if not tier2Checker.available(comp.dataset):
-               print "\033[1;31mN: Dataset %s (%s) is not available on T2_CH_CERN\033[0m" % (comp.name,comp.dataset)
-           else: print "Y: Dataset %s (%s) is available on T2_CH_CERN" % (comp.name,comp.dataset)
