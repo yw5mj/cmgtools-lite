09da66a573579f8b1a5cd4292e6f5119ee991fc5
Hengne <Hengne.Li@CERN.CH>
2016-06-05 18:13:26 +0200

    pu mb xsec scan update

--- XZZ2l2nu/tools/pileupweight/PUSkim/print_mb_list.sh
+++ XZZ2l2nu/tools/pileupweight/PUSkim/print_mb_list.sh
@@ -3,4 +3,6 @@
 
 echo `ls pileup_MC_80x*.root | sed "s/pileup_MC_80x_/\"/g" | sed "s/.root/\",/g" `
 
+echo `ls pileup_MC_80x*.root | sed "s/pileup_MC_80x_//g" | sed "s/.root/,/g" `
+
 
---
+++ XZZ2l2nu/tools/recoilFit/metSkim/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+./skimming.exe  /data/XZZ/80X_Ntuple/80X_20160603_Skim/DYJetsToLL_M50.root /data/XZZ/80X_Ntuple/80X_20160603_Skim/DYJetsToLL_M50_PUScanZPT.root 1 1
+#./skimming.exe  /data/XZZ/80X_Ntuple/80X_20160603_Skim/DYJetsToLL_M50_PUScan.root /data/XZZ/80X_Ntuple/80X_20160603_Skim/DYJetsToLL_M50_PUScanZPT.root 1 1
+
+
+#./skimming.exe  /data/XZZ/80X_Ntuple/80X_20160603_Skim/DYJetsToLL_M50.root /data/XZZ/80X_Ntuple/80X_20160603_Skim/DYJetsToLL_M50_ZPT.root 1 1
