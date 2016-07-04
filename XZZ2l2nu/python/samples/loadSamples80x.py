faf7fbeb6de1690edc1e8e3788b911f6bb139911
Hengne <Hengne.Li@CERN.CH>
2016-07-04 09:26:58 +0200

    update to 3.99fb-1 2016b new dataset

--- XZZ2l2nu/python/samples/loadSamples80x.py
+++ XZZ2l2nu/python/samples/loadSamples80x.py
@@ -119,8 +119,9 @@ dataSamples=SingleMuon+SingleElectron #+jercRefdt
 # JSON
 silverJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_Silver.txt'
 #goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-273730_13TeV_PromptReco_Collisions16_JSON.txt'
-goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt'
-run_range = (271036,274443)
+#goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt'
+goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON.txt'
+run_range = (271036,275125)
 
 jsonFile = goldenJson
 
