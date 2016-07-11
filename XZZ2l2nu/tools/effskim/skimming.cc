aa2afd8f6dcd00ea0ea4bf66740d91330608589f
Yanchu Wang <1196952700@qq.com>
2016-07-11 17:21:11 +0200

    80X method

--- XZZ2l2nu/tools/effskim/skimming.cc
+++ XZZ2l2nu/tools/effskim/skimming.cc
@@ -108,14 +108,13 @@ int main(int argc, char** argv) {
       errdt2=isoeta->GetBinError(isoeta->FindBin(llnunu_l1_l2_eta));
       isosfall=effdt1*effdt2;
       isosfallerr=TMath::Power((TMath::Power(effdt1*errdt2,2)+TMath::Power(errdt1*effdt2,2)),.5);
-      if(isosfall==0)isosfall==1;
+
       effdt1=mul1pteta->GetBinContent(mul1pteta->FindBin(llnunu_l1_l1_pt,abs(llnunu_l1_l1_eta)))/100;
       effdt2=mul2pteta->GetBinContent(mul2pteta->FindBin(llnunu_l1_l2_pt,abs(llnunu_l1_l2_eta)))/100;
       errdt1=mul1pteta->GetBinError(mul1pteta->FindBin(llnunu_l1_l1_pt,abs(llnunu_l1_l1_eta)))/100;
       errdt2=mul2pteta->GetBinError(mul2pteta->FindBin(llnunu_l1_l2_pt,abs(llnunu_l1_l2_eta)))/100;
       trgsfall=effdt1+effdt2-effdt1*effdt2;
       trgsfallerr=TMath::Power((TMath::Power((1-effdt1)*errdt2,2)+TMath::Power((1-effdt2)*errdt1,2)),.5);
-      if(trgsfall==0)trgsfall=1;
     }
     if(abs(lpdgid)==11){
       idsfall=1;
@@ -124,7 +123,6 @@ int main(int argc, char** argv) {
       isosfallerr=0;
       trgsfall=ell1pteta->GetBinContent(ell1pteta->FindBin(llnunu_l1_l1_pt,abs(llnunu_l1_l1_eta)))/100;
       trgsfallerr=ell1pteta->GetBinError(ell1pteta->FindBin(llnunu_l1_l1_pt,abs(llnunu_l1_l1_eta)))/100;
-      if(trgsfall==0)trgsfall=1;
     }
 
     tree_out->Fill();
