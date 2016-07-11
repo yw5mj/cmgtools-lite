c731505eb17c92260bbfb8a61c82d70b088cfd81
Hengne <Hengne.Li@CERN.CH>
2016-07-11 19:04:16 +0200

    add a couple of materials for pileup mb xsec scan and beamspot studies

--- XZZ2l2nu/python/samples/loadSamples80x.py
+++ XZZ2l2nu/python/samples/loadSamples80x.py
@@ -96,8 +96,8 @@ mcSamples = signalSamples + backgroundSamples + jercRefMC + extraBackgroundMC
 #mcSamples = backgroundSamples
 
 # data
-SingleMuon=[SingleMuon_Run2016B_PromptReco,SingleMuon_Run2016B_PromptReco_v2]
-SingleElectron=[SingleElectron_Run2016B_PromptReco,SingleElectron_Run2016B_PromptReco_v2]
+SingleMuon=[SingleMuon_Run2016B_PromptReco,SingleMuon_Run2016B_PromptReco_v2,SingleMuon_Run2016C_PromptReco_v2]
+SingleElectron=[SingleElectron_Run2016B_PromptReco,SingleElectron_Run2016B_PromptReco_v2,SingleElectron_Run2016C_PromptReco_v2]
 #SinglePhoton=[SinglePhoton_Run2016B_PromptReco,SinglePhoton_Run2016B_PromptReco_v2]
 #MuEG=[MuonEG_Run2016B_PromptReco,MuonEG_Run2016B_PromptReco_v2]
 
@@ -120,8 +120,9 @@ dataSamples=SingleMuon+SingleElectron #+jercRefdt
 silverJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_Silver.txt'
 #goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-273730_13TeV_PromptReco_Collisions16_JSON.txt'
 #goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt'
-goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON.txt'
-run_range = (271036,275125)
+#goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON.txt'
+goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-275783_13TeV_PromptReco_Collisions16_JSON.txt'
+run_range = (271036,275783)
 
 jsonFile = goldenJson
 
