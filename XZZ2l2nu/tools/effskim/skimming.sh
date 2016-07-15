3e228f5341f0f764a5458bedacf38daefd1cf76f
Hengne <Hengne.Li@CERN.CH>
2016-07-15 10:21:46 +0200

    put model building materials of eff scale factors into the package

--- XZZ2l2nu/tools/effskim/skimming.sh
+++ XZZ2l2nu/tools/effskim/skimming.sh
@@ -11,16 +11,20 @@ selection="(1)"
 g++ skimming.cc -o skimming.exe `root-config --cflags` `root-config --libs`
 
 #inputs
+inputdir=/dataf/heli/XZZ/80X_20160705_L6p26_Skim
+outputdir=/datad/heli/XZZ/80X_20160705_L6p26_Skim_ZpTLepTSkim
 #inputdir=/data/XZZ/76X_Ntuple/76X_20160514
-inputdir=/data/XZZ/80X_Ntuple/80X_20160705_Skim
+#inputdir=/data/XZZ/80X_Ntuple/80X_20160705_Skim
 #inputdir=/data/mewu/76X_new
 #inputdir=/afs/cern.ch/work/m/mewu/public/76X_new
 #outputdir=AnalysisRegion
-outputdir=/data2/yanchu/80X_Ntuple/80X_20160705_ZPTSkim
+#outputdir=/data2/yanchu/80X_Ntuple/80X_20160705_ZPTSkim
 mkdir -p ${outputdir}
 
-#for infile in $inputdir/*/vvTreeProducer/tree.root ; 
-for infile in $(ls $inputdir/*.root|grep -v 2016B|grep -v DYJetsToLL|grep -v BulkGrav)
+njob="0"
+
+#for infile in $(ls $inputdir/*.root|grep -v 2016B|grep -v DYJetsToLL|grep -v BulkGrav);
+for infile in  $inputdir/*.root ;
 do
 #  echo "+++ skimming $infile +++"
   outfile="${outputdir}/${infile/$inputdir\//}"
@@ -28,6 +32,12 @@ do
 
   echo -- Command: ./skimming.exe $infile $outfile 
 
-  ./skimming.exe $infile $outfile
+  ./skimming.exe $infile $outfile &> ${outfile}.log &
+
+  njob=$(( njob + 1 ))
+  if [ "$njob" -eq "100" ]; then
+    wait
+    njob="0"
+  fi
 done
 
