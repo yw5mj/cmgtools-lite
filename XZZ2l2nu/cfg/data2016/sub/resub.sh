aaa35989d8111a3a9885d2c8ef4ca7869b6e895e
Hengne <Hengne.Li@CERN.CH>
2016-08-11 00:10:44 +0200

    continue fix tunep problem, add new scripts for lsf job managements

---
+++ XZZ2l2nu/cfg/data2016/sub/resub.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+dir="dt1"
+queue="2nd"
+
+cd $dir
+list=`heppy_check.py * | grep "not" | awk {'print $1'}`
+
+listjobs=`bjobs -o "name" | grep -v JOB_NAME`
+
+for dd in $list;
+do
+  echo "### check job $dd"
+  ss=`echo $listjobs | grep "$dd "`"_has"
+  #echo " # ss=$ss "
+  #echo " # dd=$dd "
+
+  if [ "$ss" == "_has" ]
+  then
+    echo "  submit $dd"
+    cd $dd
+    echo "  bsub -q $queue -J $dd  < batchScript.sh"
+    bsub -q $queue -J $dd  < batchScript.sh
+    cd ../
+  else
+    echo "  job $dd is still running"
+  fi
+done 
+
+cd ../
+
---
+++ XZZ2l2nu/cfg/mc80x/sub/check.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+dir=mc2
+out=/data2/XZZ2/80X_20160810_Chunks
+
+# first do a overall sync
+
+rsync -var $dir $out/
+
+# go to check each directory
+cd $dir
+
+list=`ls -1 -d */`
+
+for job in $list;
+do
+  job=${job%%/}
+  ss=`heppy_check.py $job | grep "not" | awk {'print $1'}`
+  echo "## check $job "
+  if [ "$ss" == "$job" ];
+  then
+    echo "- job is not finished or has problem to be resubmitted .. "
+  else
+    echo "- job finished successfully copy out and delete .. "
+    echo " > rsync -var $job $out/$dir/"
+    rsync -var $job $out/$dir/
+    echo " > rm -rf $job"
+    rm -rf $job
+  fi
+
+done
+
+cd -
+
+
