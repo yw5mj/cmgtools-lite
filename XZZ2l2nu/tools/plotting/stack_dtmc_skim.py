4ef2d47dce6bc5bc080d4d60251bb40f6a57dc9c
Hengne <Hengne.Li@CERN.CH>
2016-09-02 11:29:47 +0200

    update electron trigger eff sf input file with more binning from yanchu

--- XZZ2l2nu/tools/plotting/stack_dtmc_skim.py
+++ XZZ2l2nu/tools/plotting/stack_dtmc_skim.py
@@ -7,12 +7,13 @@ from CMGTools.XZZ2l2nu.plotting.MergedPlotter import MergedPlotter
 from CMGTools.XZZ2l2nu.plotting.StackPlotter import StackPlotter
 
 
+tag="TightZPt100_BigDY_Sept1Recoil_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
 #tag="LODY_Sept1RecoilNoSmooth_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
 #tag="LODY_Sept1RecoilSmooth_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
 #tag="NoElTrgEff_Sept1RecoilNoSmooth_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
 #tag="Sept1RecoilSmooth_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
 #tag="LODY_Sept1Recoil_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
-tag="BigDY_Sept1RecoilNoSmooth_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
+#tag="BigDY_Sept1RecoilNoSmooth_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
 #tag="BigDY_Sept1RecoilSmooth_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
 #tag="BigDY_Sept1Recoil_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_"
 #tag="NewRecoilNoPUWtNoSmooth_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80X_L12p9_HLTv2_ichepPU_ZPTwt_allSF_"
@@ -36,15 +37,15 @@ tag="BigDY_Sept1RecoilNoSmooth_GenCutZPtwt_ElEscale_NewPuMuSf_MuPtScale_NTgEf_80
 #tag="test_"
 #tag=""
 #cutChain='loosecut'
-cutChain='tight'
-#cutChain='tightzpt100'
+#cutChain='tight'
+cutChain='tightzpt100'
 #cutChain='tightzpt100met50'
 #cutChain='tightzpt100met100'
 #cutChain='tightzpt100met200'
 
 # can be el or mu or both
 channel='el' 
-LogY=True
+LogY=False
 test=False
 DrawLeptons=True
 doRhoScale=True
@@ -255,9 +256,9 @@ WJets.setFillProperties(1001,ROOT.kBlue-6)
 
 
 zjetsPlotters=[]
-zjetsSamples = ['DYJetsToLL_M50_RecoilNoSmooth','DYJetsToLL_M50_MGMLM_Ext1_RecoilNoSmooth']
+#zjetsSamples = ['DYJetsToLL_M50_RecoilNoSmooth','DYJetsToLL_M50_MGMLM_Ext1_RecoilNoSmooth']
 #zjetsSamples = ['DYJetsToLL_M50_RecoilSmooth','DYJetsToLL_M50_MGMLM_Ext1_RecoilSmooth']
-#zjetsSamples = ['DYJetsToLL_M50','DYJetsToLL_M50_MGMLM_Ext1']
+zjetsSamples = ['DYJetsToLL_M50','DYJetsToLL_M50_MGMLM_Ext1']
 #zjetsSamples = ['DYJetsToLL_M50'] # M50
 #zjetsSamples = ['DYJetsToLL_M50_RecoilSmooth'] # M50
 #zjetsSamples = ['DYJetsToLL_M50_RecoilNoSmooth'] # M50
--- XZZ2l2nu/tools/triggereff/trigeff_elec.C
+++ XZZ2l2nu/tools/triggereff/trigeff_elec.C
@@ -1,21 +1,20 @@
 {
 
-  TFile* fin= new TFile("/home/heli/XZZ/80X_20160825_light/SingleEMU_Run2016BCD_PromptReco/vvTreeProducer/tree.root");
+  TFile* fin= new TFile("/home/heli/XZZ/80X_20160825_light_Skim/SingleEMU_Run2016BCD_PromptReco.root");
   std::string sel_tight = "(abs(llnunu_l1_l1_pdgId)==11&&abs(llnunu_l1_l2_pdgId)==11&&llnunu_l1_l1_pt>115&&abs(llnunu_l1_l1_eta)<2.5&&llnunu_l1_l2_pt>35&&abs(llnunu_l1_l2_eta)<2.5&&llnunu_l1_mass>70&&llnunu_l1_mass<110)";
   std::string sel_loose = "(abs(llnunu_l1_l1_pdgId)==11&&abs(llnunu_l1_l2_pdgId)==11&&llnunu_l1_l1_pt>35&&abs(llnunu_l1_l1_eta)<2.5&&llnunu_l1_l2_pt>35&&abs(llnunu_l1_l2_eta)<2.5&&llnunu_l1_mass>70&&llnunu_l1_mass<110)";
   TTree* tree = (TTree*)fin->Get("tree");
 
   std::string sel_passHLT = "(HLT_ELEv2)";
 
-  std::string sel_pass;
-  std::string sel_base;
+  //sel_tight += "&&(llnunu_l1_pt>200)";
 
   std::string sel_pass_tight = "("+sel_tight+"&&"+sel_passHLT+")";
   std::string sel_base_tight = sel_tight;
 
 
-  double N_pass_tight = (double)tree->GetEntries(sel_pass.c_str());
-  double N_base_tight = (double)tree->GetEntries(sel_base.c_str());
+  double N_pass_tight = (double)tree->GetEntries(sel_pass_tight.c_str());
+  double N_base_tight = (double)tree->GetEntries(sel_base_tight.c_str());
 
   std::cout << " N_pass_tight = " << N_pass_tight << "; N_base_tight = " << N_base_tight << ";  eff = " << N_pass_tight/N_base_tight << std::endl;
 
